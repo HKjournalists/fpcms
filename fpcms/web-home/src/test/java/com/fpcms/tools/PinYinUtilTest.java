d736a11309fcecb9352fa8d31cd9409387e7af7f
badqiu <badqiu@gmail.com>
2012-12-27 01:50:55 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/web-home/src/test/java/com/fpcms/tools/PinYinUtilTest.java
+++ fpcms/web-home/src/test/java/com/fpcms/tools/PinYinUtilTest.java
@@ -1,85 +1,85 @@
 package com.fpcms.tools;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.junit.Assert;
-import org.junit.Test;
-import org.springframework.util.ResourceUtils;
-
-import com.fpcms.common.util.SpringContext;
-import com.fpcms.model.CmsSite;
-import com.fpcms.service.CmsSiteService;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.LinkedHashSet;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import org.apache.commons.io.IOUtils;
+//import org.apache.commons.lang.StringUtils;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.springframework.util.ResourceUtils;
+//
+//import com.fpcms.common.util.SpringContext;
+//import com.fpcms.model.CmsSite;
+//import com.fpcms.service.CmsSiteService;
 
 public class PinYinUtilTest {
-	
-	@Test
-	public void test() throws Exception {
-		Set<String> citys = getAllCitys();
-		
-		Map pinyinMap = new HashMap();
-		Map cityMap = new HashMap();
-		for(String city : citys) {
-			String firstSpell = PinyinUtil.cn2FirstSpell(city);
-			pinyinMap.put(firstSpell, city);
-			cityMap.put(city, firstSpell);
-			System.out.println(firstSpell+" ==> " + city);
-		}
-		Assert.assertEquals(cityMap.toString(),pinyinMap.size() , cityMap.size());
-	}
-	
-	@Test 
-	public void test_create_site_by_city_list() throws FileNotFoundException, IOException {
-		CmsSiteService service = SpringContext.getBean(CmsSiteService.class);
-		Set<String> citys = getAllCitys();
-		for(String city : citys) {
-			int count = 0;
-			String cityPinyin = PinyinUtil.cn2FirstSpell(city);
-			String domain = ".aaafaipiao.com";
-			for(int i = 0; i < 10; i++) {
-				try {
-					String finalCityPinyin = i == 0 ? cityPinyin : cityPinyin+i;
-					service.create(newCmsSite(city, finalCityPinyin,domain));
-					System.out.println("create city:"+city+" => "+finalCityPinyin);
-					break;
-				}catch(org.springframework.dao.DuplicateKeyException e) {
-				}
-			}
-			
-		}
-	}
-
-	private CmsSite newCmsSite(String city, String cityPinyin,String domain) {
-		CmsSite cmsSite = new CmsSite();
-		cmsSite.setSiteDomain(cityPinyin+domain);
-		cmsSite.setSiteName(city);
-		cmsSite.setSiteKeyword(city+"开发票 "+city+"代开发票");
-		cmsSite.setSiteCity(cityPinyin);
-		cmsSite.setRemarks(null);
-		return cmsSite;
-	}
-
-	private Set<String> getAllCitys() throws FileNotFoundException, IOException {
-		File file = ResourceUtils.getFile("classpath:city.txt");
-		FileReader reader = new FileReader(file);
-		Set<String> citys = new LinkedHashSet<String>();
-		for(String line : IOUtils.readLines(reader)) {
-			String[] array = line.split("\\s+");
-			for(String city : array) {
-				if(StringUtils.isNotBlank(city)) {
-					citys.add(city);
-				}
-			}
-		}
-		reader.close();
-		return citys;
-	}
+//	
+//	@Test
+//	public void test() throws Exception {
+//		Set<String> citys = getAllCitys();
+//		
+//		Map pinyinMap = new HashMap();
+//		Map cityMap = new HashMap();
+//		for(String city : citys) {
+//			String firstSpell = PinyinUtil.cn2FirstSpell(city);
+//			pinyinMap.put(firstSpell, city);
+//			cityMap.put(city, firstSpell);
+//			System.out.println(firstSpell+" ==> " + city);
+//		}
+//		Assert.assertEquals(cityMap.toString(),pinyinMap.size() , cityMap.size());
+//	}
+//	
+//	@Test 
+//	public void test_create_site_by_city_list() throws FileNotFoundException, IOException {
+//		CmsSiteService service = SpringContext.getBean(CmsSiteService.class);
+//		Set<String> citys = getAllCitys();
+//		for(String city : citys) {
+//			int count = 0;
+//			String cityPinyin = PinyinUtil.cn2FirstSpell(city);
+//			String domain = ".aaafaipiao.com";
+//			for(int i = 0; i < 10; i++) {
+//				try {
+//					String finalCityPinyin = i == 0 ? cityPinyin : cityPinyin+i;
+//					service.create(newCmsSite(city, finalCityPinyin,domain));
+//					System.out.println("create city:"+city+" => "+finalCityPinyin);
+//					break;
+//				}catch(org.springframework.dao.DuplicateKeyException e) {
+//				}
+//			}
+//			
+//		}
+//	}
+//
+//	private CmsSite newCmsSite(String city, String cityPinyin,String domain) {
+//		CmsSite cmsSite = new CmsSite();
+//		cmsSite.setSiteDomain(cityPinyin+domain);
+//		cmsSite.setSiteName(city);
+//		cmsSite.setSiteKeyword(city+"开发票 "+city+"代开发票");
+//		cmsSite.setSiteCity(cityPinyin);
+//		cmsSite.setRemarks(null);
+//		return cmsSite;
+//	}
+//
+//	private Set<String> getAllCitys() throws FileNotFoundException, IOException {
+//		File file = ResourceUtils.getFile("classpath:city.txt");
+//		FileReader reader = new FileReader(file);
+//		Set<String> citys = new LinkedHashSet<String>();
+//		for(String line : IOUtils.readLines(reader)) {
+//			String[] array = line.split("\\s+");
+//			for(String city : array) {
+//				if(StringUtils.isNotBlank(city)) {
+//					citys.add(city);
+//				}
+//			}
+//		}
+//		reader.close();
+//		return citys;
+//	}
 }
--- fpcms/web-home/src/test/java/com/fpcms/tools/PinyinUtil.java
+++ fpcms/web-home/src/test/java/com/fpcms/tools/PinyinUtil.java
@@ -1,12 +1,12 @@
 package com.fpcms.tools;
 
-import java.io.UnsupportedEncodingException;
-
-import net.sourceforge.pinyin4j.PinyinHelper;
-import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
-import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
-import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
-import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+//import java.io.UnsupportedEncodingException;
+//
+//import net.sourceforge.pinyin4j.PinyinHelper;
+//import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+//import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+//import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+//import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
 /** 
 * 拼音工具 
@@ -14,65 +14,65 @@ import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombi
 * @author leizhimin 2009-7-15 15:26:21 
 */ 
 public class PinyinUtil { 
-
-        /** 
-         * 获取汉字串拼音首字母，英文字符不变 
-         * 
-         * @param chinese 汉字串 
-         * @return 汉语拼音首字母 
-         */ 
-        public static String cn2FirstSpell(String chinese) { 
-                StringBuffer pybf = new StringBuffer(); 
-                char[] arr = chinese.toCharArray(); 
-                HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat(); 
-                defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE); 
-                defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE); 
-                for (int i = 0; i < arr.length; i++) { 
-                        if (arr[i] > 128) { 
-                                try { 
-                                        String[] _t = PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat); 
-                                        if (_t != null) { 
-                                                pybf.append(_t[0].charAt(0)); 
-                                        } 
-                                } catch (BadHanyuPinyinOutputFormatCombination e) { 
-                                        e.printStackTrace(); 
-                                } 
-                        } else { 
-                                pybf.append(arr[i]); 
-                        } 
-                } 
-                return pybf.toString().replaceAll("\\W", "").trim(); 
-        } 
-
-        /** 
-         * 获取汉字串拼音，英文字符不变 
-         * 
-         * @param chinese 汉字串 
-         * @return 汉语拼音 
-         */ 
-        public static String cn2Spell(String chinese) { 
-                StringBuffer pybf = new StringBuffer(); 
-                char[] arr = chinese.toCharArray(); 
-                HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat(); 
-                defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE); 
-                defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE); 
-                for (int i = 0; i < arr.length; i++) { 
-                        if (arr[i] > 128) { 
-                                try { 
-                                        pybf.append(PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat)[0]); 
-                                } catch (BadHanyuPinyinOutputFormatCombination e) { 
-                                        e.printStackTrace(); 
-                                } 
-                        } else { 
-                                pybf.append(arr[i]); 
-                        } 
-                } 
-                return pybf.toString(); 
-        } 
-
-        public static void main(String[] args) throws UnsupportedEncodingException { 
-                String x = "中国--abc--人民--diy123--银行"; 
-                System.out.println(cn2FirstSpell(x)); 
-                System.out.println(cn2Spell(x)); 
-        } 
+//
+//        /** 
+//         * 获取汉字串拼音首字母，英文字符不变 
+//         * 
+//         * @param chinese 汉字串 
+//         * @return 汉语拼音首字母 
+//         */ 
+//        public static String cn2FirstSpell(String chinese) { 
+//                StringBuffer pybf = new StringBuffer(); 
+//                char[] arr = chinese.toCharArray(); 
+//                HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat(); 
+//                defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE); 
+//                defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE); 
+//                for (int i = 0; i < arr.length; i++) { 
+//                        if (arr[i] > 128) { 
+//                                try { 
+//                                        String[] _t = PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat); 
+//                                        if (_t != null) { 
+//                                                pybf.append(_t[0].charAt(0)); 
+//                                        } 
+//                                } catch (BadHanyuPinyinOutputFormatCombination e) { 
+//                                        e.printStackTrace(); 
+//                                } 
+//                        } else { 
+//                                pybf.append(arr[i]); 
+//                        } 
+//                } 
+//                return pybf.toString().replaceAll("\\W", "").trim(); 
+//        } 
+//
+//        /** 
+//         * 获取汉字串拼音，英文字符不变 
+//         * 
+//         * @param chinese 汉字串 
+//         * @return 汉语拼音 
+//         */ 
+//        public static String cn2Spell(String chinese) { 
+//                StringBuffer pybf = new StringBuffer(); 
+//                char[] arr = chinese.toCharArray(); 
+//                HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat(); 
+//                defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE); 
+//                defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE); 
+//                for (int i = 0; i < arr.length; i++) { 
+//                        if (arr[i] > 128) { 
+//                                try { 
+//                                        pybf.append(PinyinHelper.toHanyuPinyinStringArray(arr[i], defaultFormat)[0]); 
+//                                } catch (BadHanyuPinyinOutputFormatCombination e) { 
+//                                        e.printStackTrace(); 
+//                                } 
+//                        } else { 
+//                                pybf.append(arr[i]); 
+//                        } 
+//                } 
+//                return pybf.toString(); 
+//        } 
+//
+//        public static void main(String[] args) throws UnsupportedEncodingException { 
+//                String x = "中国--abc--人民--diy123--银行"; 
+//                System.out.println(cn2FirstSpell(x)); 
+//                System.out.println(cn2Spell(x)); 
+//        } 
 }
