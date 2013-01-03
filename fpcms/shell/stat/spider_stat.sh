26f5d853d3139b96f8e01c72ef941337070e2b41
badqiu <badqiu@gmail.com>
2013-01-03 12:56:13 +0800

    badqiu

--- fpcms/shell/stat/spider_stat.sh
+++ fpcms/shell/stat/spider_stat.sh
@@ -1,1 +1,10 @@
-cat /data/log/fpcms/spider.log| grep baidu | awk '{print $4}'
+#!/bin/bash
+
+echo "----------- Baidu Visit stat -----------"
+cat /data/log/fpcms/spider.log| grep baidu | awk '{print $4}' | sort | uniq
+
+echo ""
+echo ""
+echo ""
+echo "----------- Google Visit stat -----------"
+cat /data/log/fpcms/spider.log| grep google | awk '{print $4}' | sort | uniq
