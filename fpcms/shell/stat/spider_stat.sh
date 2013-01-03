89b86a5a2801935dfc8814bfb842af8cde58063b
badqiu <badqiu@gmail.com>
2013-01-03 15:29:34 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/shell/stat/spider_stat.sh
+++ fpcms/shell/stat/spider_stat.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 echo "----------- Baidu Visit stat -----------"
-cat /data/log/fpcms/spider.log| grep baidu | awk '{print $4}' | sort | uniq
+cat /data/log/fpcms/spider.log | grep baidu | awk '{print $4}' | sort | uniq
 
 echo ""
 echo ""
 echo ""
 echo "----------- Google Visit stat -----------"
-cat /data/log/fpcms/spider.log| grep google | awk '{print $4}' | sort | uniq
+cat /data/log/fpcms/spider.log | grep google | awk '{print $4}' | sort | uniq
+
+
+echo "/data/log/fpcms/spider.log"
+
+tail -n 30 /data/log/fpcms/spider.log
+
