0447488009b8eff7e4f0a25d3aef369854e36549
badqiu <badqiu@gmail.com>
2013-01-11 00:46:42 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/util/src/main/java/com/fpcms/common/util/CityUtil.java
+++ fpcms/util/src/main/java/com/fpcms/common/util/CityUtil.java
@@ -188,6 +188,33 @@ public class CityUtil {
 		public void setProvincePinyin(String provincePinyin) {
 			this.provincePinyin = provincePinyin;
 		}
+		
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((city == null) ? 0 : city.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			City other = (City) obj;
+			if (city == null) {
+				if (other.city != null)
+					return false;
+			} else if (!city.equals(other.city))
+				return false;
+			return true;
+		}
+
 		@Override
 		public String toString() {
 			return "City [rank=" + rank + ", gdp=" + gdp + ", city=" + city
--- fpcms/web-home/src/main/webapp/layout.jsp
+++ fpcms/web-home/src/main/webapp/layout.jsp
@@ -65,18 +65,6 @@
 	<div class="width mt10">
 		<div id="page_left">
 			<duowan:block name="page_left">
-			<div class="border mt10" style="font-size: 15pt">
-				<div class="subject_bg">
-					<div class="subject_title"><font color="red" style="font-size: 15pt">联系我们</font></div>
-				</div>
-				<div class="m10 " >
-					<div align="left" >
-					<b>联系人:</b> ${contactName} <br/>
-					<b>电话:</b>&nbsp;&nbsp;&nbsp;${mobile} <br/>
-					<b>QQ:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${qq}<a href="http://wpa.qq.com/msgrd?v=3&amp;uin=${qq}&amp;site=${requestHost}&amp;menu=yes"><img src="http://wpa.qq.com/pa?p=2:${qq}:41" border="0" /></a>
-					</div>
-				</div>
-			</div>
 			</duowan:block>
 		</div>
 
