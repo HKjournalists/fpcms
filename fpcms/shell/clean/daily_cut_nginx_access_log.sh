f12922c028416d6c0586fb001bf231d050a212ce
badqiu <badqiu@gmail.com>
2013-01-16 21:54:13 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/shell/clean/daily_cut_nginx_access_log.sh
+++ fpcms/shell/clean/daily_cut_nginx_access_log.sh
@@ -2,6 +2,7 @@
 
 day=`date +%Y-%m-%d`
 mv /data/log/nginx/access.log /data/log/nginx/access.log.${day}
+mv /data/log/nginx/error.log /data/log/nginx/error.log.${day}
 
 # send sign for renew access log
 kill -USR1 `cat /var/run/nginx.pid`
