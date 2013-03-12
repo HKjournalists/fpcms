d88000e15885a36439597ff61f450615d2ca37c6
badqiu <badqiu@gmail.com>
2013-03-12 10:16:15 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/dao/src/main/java/com/fpcms/model/CmsSitePropertyEnum.java
+++ fpcms/dao/src/main/java/com/fpcms/model/CmsSitePropertyEnum.java
@@ -2,7 +2,10 @@ package com.fpcms.model;
 
 public enum CmsSitePropertyEnum {
 	PROP_KEYWORDS_RANK_BAIDU("keywordsRankBaidu","百度关键字排名"),
-	PROP_EXPECT_CONTENT_TAGS("exceptContentTags","期待的网站内容tag");
+	PROP_EXPECT_CONTENT_TAGS("exceptContentTags","期待的网站内容tag"),
+	WEB_META_VERIFY_CODE("webMetaVerifyCode","验证网站归属的Html Head Meta"),
+	WEBSITE_STAT_CODE("websiteStatCode","网站访问统计代码"),
+	;
 	
 	private final String code;
 	private final String desc;
