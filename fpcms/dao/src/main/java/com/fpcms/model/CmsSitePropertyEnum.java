e2fb2e9bee2d8ec134d5260c73b425060e7ac91c
badqiu <badqiu@gmail.com>
2013-03-18 08:25:38 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/dao/src/main/java/com/fpcms/model/CmsSitePropertyEnum.java
+++ fpcms/dao/src/main/java/com/fpcms/model/CmsSitePropertyEnum.java
@@ -4,17 +4,23 @@ import com.duowan.common.lang.enums.EnumBase;
 
 public enum CmsSitePropertyEnum implements EnumBase{
 	PROP_KEYWORDS_RANK_BAIDU("keywordsRankBaidu","百度关键字排名"),
-	PROP_EXPECT_CONTENT_TAGS("exceptContentTags","期待的网站内容tag"),
+	PROP_EXPECT_CONTENT_TAGS("exceptContentTags","期待的网站内容tag",true),
 	WEB_META_VERIFY_CODE("webMetaVerifyCode","验证网站归属的Html Head Meta"),
 	WEBSITE_STAT_CODE("websiteStatCode","网站访问统计代码"),
 	;
 	
 	private final String code;
 	private final String desc;
+	private final boolean required;
 	
-	CmsSitePropertyEnum(String code,String desc) {
+	CmsSitePropertyEnum(String code,String desc,boolean required) {
 		this.code = code;
 		this.desc = desc;
+		this.required = required;
+	}
+	
+	CmsSitePropertyEnum(String code,String desc) {
+		this(code,desc,false);
 	}
 	
 	public String getCode() {
@@ -23,6 +29,10 @@ public enum CmsSitePropertyEnum implements EnumBase{
 	
 	public String getDesc() {
 		return desc;
+	}
+
+	public boolean isRequired() {
+		return required;
 	}
 	
 }
