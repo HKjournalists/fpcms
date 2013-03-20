ae8f3fcdc3b9b567abe38944972287dd6b186028
badqiu <badqiu@gmail.com>
2013-03-20 22:04:51 +0800

    Signed-off-by: badqiu <badqiu@gmail.com>

--- fpcms/util/src/test/java/com/fpcms/common/util/BlogUtilTest.java
+++ fpcms/util/src/test/java/com/fpcms/common/util/BlogUtilTest.java
@@ -15,6 +15,8 @@ public class BlogUtilTest extends Assert{
 		printAndAssert("http://www.blogjava.net/badqiu",false);
 		printAndAssert("http://blog.sina.com.cn/u/3225400392",false);
 //		printAndAssert("http://sh292did.blog.163.com/",true);
+//		printAndAssert("http://blog.csdn.net/fpqqchao2",true);
+		printAndAssert("http://www.cnblogs.com/fpqqchao/",true);
 	}
 
 	private void printAndAssert(String blogUrl,boolean ping) {
